0e515317821cb4b2e34f2f6669d3f838eb802a4b
Gaëtan PERRIER <gaetanperrier8@gmail.com>
2026-07-03 17:24:59 +0200

    first commit

---
+++ vars/generateEnv.groovy
@@ -0,0 +1,17 @@
+def call() {
+
+    withCredentials([
+        string(credentialsId: "APP_ENV", variable: 'APP_ENV'),
+        string(credentialsId: "APP_NAME", variable: 'APP_NAME'),
+        string(credentialsId: "APP_PORT" variable: 'PORT')
+    ]) {
+
+        sh """
+            cat > .env <<EOF
+            APP_NAME=${APP_NAME}
+            APP_ENV=${APP_ENV}
+            PORT=${PORT}
+            EOF
+        """
+    }
+}
